2b086798dec9c22cd024f94ac5aa08ad9daf5e19
Ian Alexander <iralex255@gmail.com>
2019-06-08 08:53:29 -0700

    get 10 questions displaying alert results

--- js/scripts.js
+++ js/scripts.js
@@ -12,25 +12,14 @@ $(document).ready(function() {
     var question10 = parseInt($("#question10").val());
 
       if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 === 10) {
-       alert("You ranked a 10/10");
-     } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 === 9) {
-      alert("You ");
-    } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 === 8 {
-     alert("You ");
-   } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 ===7) {
-    alert("You ");
-  } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 ===6) {
-   alert("You ");
- } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 ===5) {
-     alert("4/10");
-   } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 ===4) {
-     alert("1/10");
-   } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 ===3) {
-    alert("You ");
-  } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 ===2) {
-   alert("You ");
- } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 ===1) {
-  alert("You ");
+        alert("You scored a 10/10. You are in a unique positon to use your social privilges to help those around you in an extremely meaningful way!");
+      } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 >7) {
+        alert("You scored in the top teir of the social strata.");
+      } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 >4) {
+        alert("You placed around the middle.");
+      } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 >=0) {
+      alert("You appear to experience the negative ramifications of the capitalist-imperialist-white supremecist-patriarchy.");
+    }
     event.preventDefault();
   });
 });
