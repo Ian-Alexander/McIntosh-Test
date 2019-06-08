684697859728baff174f14ac41d0892c20abbada
Ian Alexander <iralex255@gmail.com>
2019-06-07 17:19:30 -0700

    it will get basic back end functioning for replication

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,30 @@
-
 $(document).ready(function() {
   $("form#privilege").submit(function(event) {
-    alert('got to begining of doc!');
-    var question1 = parseInt($("input[type=radio][name=question1]:checked").val());
-    var question2 = parseInt($("input[type=radio][name=question2]:checked").val());
-    var question3 = parseInt($("input[type=radio][name=question3]:checked").val());
+    alert("1");
+    var question1 = parseInt($("#question1").val());
+    var question2 = parseInt($("#question2").val());
+    var question3 = parseInt($("#question3").val());
+    var question4 = parseInt($("#question4").val());
+    var question5 = parseInt($("#question5").val());
+    var question6 = parseInt($("#question6").val());
+    var question7 = parseInt($("#question7").val());
+    var question8 = parseInt($("#question8").val());
+    var question9 = parseInt($("#question9").val());
+    var question10 = parseInt($("#question10").val());
 
-    if ("question1 + question2 + question3 === 3"){
-      $("#quote").show();
-    };
-      event.preventDefault();
-    alert("end of doc")
+      if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 === 9) {
+       alert("you did it");
+     };
+     // } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 >= 7 && < 10) {
+     //      alert("you did it again");
+     //    } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 >= 4){
+     //      $("#quote3").show;
+     //    } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 >= 2){
+     //      $("#quote4").show;
+     //    } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 === 1){
+     //      $("#quote5").show;
+     //    };
+    event.preventDefault();
+     alert("end");
   });
 });
