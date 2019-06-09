25e2b3b1cdad42c6002e42e7e9019346b077f792
Ian Alexander <iralex255@gmail.com>
2019-06-09 09:26:51 -0700

    add README, complete styling and formatting.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+var name = prompt("Please enter your name:")
+
 $(document).ready(function() {
   $("form#privilege").submit(function(event) {
     var question1 = parseInt($("#question1").val());
@@ -12,13 +14,13 @@ $(document).ready(function() {
     var question10 = parseInt($("#question10").val());
 
       if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 === 10) {
-        alert("You scored a 10/10. You are in a unique positon to use your social privilges to help those around you in an extremely meaningful way!");
+        alert(name +", you scored a 10/10. You are in a unique positon to use your social privilges to help those around you in an extremely meaningful way! Having white privilege does not mean that you are racist or bad, it means that you have a duty. Use all of the tools in your toolbox to help those with less.");
       } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 >7) {
-        alert("You scored in the top teir of the social strata.");
+        alert(name + ", you scored in the top teir of the social strata. People in this category hold an immense amount of power to help uplift those with less privileges around them. Consider those who check off more boxes than you did, and also know its okay to demand more from those who checked even less boxes than you.");
       } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 >4) {
-        alert("You placed around the middle.");
+        alert(name + ", you placed around the middle. You may feel like you are stuck between having a challenging existance, and having a privileged one, which can be tricky to navigate. It's important to remember that in every oppression, there is room for resistance, and in every resistance, there is room for oppression to live as well. Nothing is either/or, but instead, both/and. Use what tools you have to help uplift others, but do not feel bad when you need to call on others to help you too.");
       } else if (question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10 >=0) {
-      alert("You appear to experience the negative ramifications of the capitalist-imperialist-white supremecist-patriarchy.");
+      alert(name +", your results show that you experience an immense amount of negative ramifications of the capitalist-imperialist-white supremecist-patriarchy. With that experience comes a deeper understanding of power and privilege than I or Peggy McIntosh could ever understand. Remeber that you do not owe anybody anything, and you are valid in all of your responses to the oppression you face.");
     }
     event.preventDefault();
   });
